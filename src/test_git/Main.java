72ab2ded24682eebfc67093731a6b59d71cd6935
hjeffar <hicham.jeffar@gmail.com>
2015-12-08 18:27:49 +0000

    Modif 2 branche 1

--- src/test_git/Main.java
+++ src/test_git/Main.java
@@ -2,6 +2,6 @@ package test_git;
 
 public class Main {
 public static void main(String[] args) {
-	System.out.println("Modif dans la branche 1");
+	System.out.println("Modif 2 dans la branche 1");
 }
 }
