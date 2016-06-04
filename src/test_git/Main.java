2245297b2c12186d7975b629af951924574d687c
hjeffar <hjeffar@sqli.com>
2016-06-04 18:25:57 +0100

    modif 3files par hicham48

--- src/test_git/Main.java
+++ src/test_git/Main.java
@@ -6,6 +6,8 @@ public static void main(String[] args) {
 	System.out.println("Git test : modif 2");
 
 	System.out.println("Modif 2 dans la branche 1");
+	
+	System.out.println("Modif 6 hicham48");
 
 }
 }
