06a85ac3cca31c2d06bfa614e89e787a529f9218
hjeffar <hjeffar@sqli.com>
2016-06-05 11:39:50 +0000

    modif7 par hicham48

--- src/test_git/Main.java
+++ src/test_git/Main.java
@@ -8,6 +8,8 @@ public static void main(String[] args) {
 	System.out.println("Modif 2 dans la branche 1");
 	
 	System.out.println("Modif 6 hicham48");
+	
+	System.out.println("Modif 7 hicham48");
 
 }
 }
